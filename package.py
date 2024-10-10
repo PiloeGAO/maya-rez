6b2e278abc9c0d48a8b05b0964cac6d103e05429
PiloeGAO <leonumerique@gmail.com>
2024-10-10 09:19:43 +0200

    fix: Maya crashing when alias is used
    The issue is from doskey.exe, the utility software used by rez to create the CMD terminal alias.

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "maya"
 
-version = "2025.2"
+version = "2025.2-r.1"
 
 authors = [
     "Autodesk"
@@ -11,21 +11,9 @@ description = \
     Animation and VFX  software.
     """
 
-tools = [
-    "maya",
-    "mayabatch",
-    "mayapy",
-    "mayarender"
-]
-
 uuid = "autodesk.maya"
 
 build_command = ""
 
 def commands():
-    env.MAYA_LOCATION = "C:\\PROGRA~1\\Autodesk\\Maya2025"
-
-    alias("maya", "C:\\PROGRA~1\\Autodesk\\Maya2025\\bin\\maya.exe")
-    alias("mayabatch", "C:\\PROGRA~1\\Autodesk\\Maya2025\\bin\\mayabatch.exe")
-    alias("mayapy", "C:\\PROGRA~1\\Autodesk\\Maya2025\\bin\\mayapy.exe")
-    alias("mayarender", "C:\\PROGRA~1\\Autodesk\\Maya2025\\bin\\Render.exe")
+    env.PATH.prepend("C:\\PROGRA~1\\Autodesk\\Maya2025\\bin")
